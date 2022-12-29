a3df5c7c1bcf6d6fdf8627595247aebca0fa832d
Tampoka <goal.thanks@gmail.com>
2022-12-29 22:15:14 +0900

    Add daisyui to config.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,4 +2,9 @@ module.exports = {
     content: ["{pages,src}/**/*.{js,ts,jsx,tsx}"],
     darkMode: "class",
     important: true, // important in prod is must be
+    theme: ["dark"],
+    plugins: [require("daisyui")],
+    daisyui: {
+        themes: [],
+    },
 }
