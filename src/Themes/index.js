21782ae3808cbb017b2b40569d0832ee94f1eb31
Tampoka <goal.thanks@gmail.com>
2022-12-29 22:26:37 +0900

    Add themes export modules.

---
+++ src/Themes/index.js
@@ -0,0 +1,11 @@
+const defaultTheme = require("./defaultTheme");
+const darkTheme = require("./darkTheme");
+const luxuryTheme = require("./luxury");
+const cupcakeTheme = require("./cupcake");
+
+module.exports = {
+    default: defaultTheme,
+    dark: darkTheme,
+    luxury: luxuryTheme,
+    cupcake: cupcakeTheme,
+};
